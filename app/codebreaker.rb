29f908e137b0dddc66eae6e7edcb8ca310f50a18
opaismoe <wiebe.buursink@gmail.com>
2017-12-16 17:01:25 +0100

    Fixed scoring system

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -16,28 +16,44 @@ class Codebreaker
 
     def guess(input)
 
-      unless input.length == 4
-        output.puts 'Try guessing a number with four digits'
-      end
-
       guessed_number = input.split("")
       secret_number_arr = @secret_number.split("")
 
+      match = 0
+      no_match = 0
+      @exact_match = 0
+
+      #match
       for num in (0..3)
-        if guessed_number[num] == secret_number_arr[num]
-          output.puts "+"
-          return secret_number_arr
+        if guessed_number.include?(secret_number_arr[num])
+          match += 1
+          #no match
+        else !guessed_number.include?(secret_number_arr[num])
+          no_match += 1
         end
       end
 
+      #exact match
       for num in (0..3)
-        if guessed_number.include?(secret_number_arr[num])
-          output.puts "-"
-        else !guessed_number.include?(secret_number_arr[num])
-          output.puts ""
+        if guessed_number[num] == secret_number_arr[num]
+          @exact_match += 1
         end
       end
-      
+
+
+
+
+      unless input.length == 4
+        output.puts 'Try guessing a number with four digits'
+      else
+        match = match - @exact_match
+        exact_score = "+"*@exact_match
+        match_score = "-"*match
+        no_match_score = ""*no_match
+        final_score = exact_score + match_score + no_match_score
+        output.puts final_score
+      end
+
     end
   end
 end
