ec99a068d93f4bb8901304384bb318c628bbc638
opaismoe <wiebe.buursink@gmail.com>
2017-12-17 14:26:52 +0100

    Refactored finale score a little

--- app/codebreaker.rb
+++ app/codebreaker.rb
@@ -2,7 +2,6 @@ class Codebreaker
   class Game
     attr_reader :output
 
-
     def initialize(output)
       @output = output
     end
@@ -14,16 +13,32 @@ class Codebreaker
       output.puts "Enter guess:"
     end
 
-    def guess(input)
+    def final_score(match, exact_match, no_match, input)
+      unless input.length == 4
+        output.puts 'Try guessing a number with four digits'
+      else
+        match = match - exact_match
+        exact_score = "+"*exact_match
+        match_score = "-"*match
+        no_match_score = ""*no_match
+        final_score = exact_score + match_score + no_match_score
+      end
+    end
 
+    def guess(input)
       guessed_number = input.split("")
       secret_number_arr = @secret_number.split("")
 
       match = 0
       no_match = 0
-      @exact_match = 0
+      exact_match = 0
 
-      #match
+      for num in (0..3)
+        if guessed_number[num] == secret_number_arr[num]
+          exact_match += 1
+        end
+      end
+
       for num in (0..3)
         if guessed_number.include?(secret_number_arr[num])
           match += 1
@@ -33,27 +48,8 @@ class Codebreaker
         end
       end
 
-      #exact match
-      for num in (0..3)
-        if guessed_number[num] == secret_number_arr[num]
-          @exact_match += 1
-        end
-      end
-
-
-
-
-      unless input.length == 4
-        output.puts 'Try guessing a number with four digits'
-      else
-        match = match - @exact_match
-        exact_score = "+"*@exact_match
-        match_score = "-"*match
-        no_match_score = ""*no_match
-        final_score = exact_score + match_score + no_match_score
-        output.puts final_score
-      end
-
+      output.puts final_score(match, exact_match, no_match, input)
+      
     end
   end
 end
